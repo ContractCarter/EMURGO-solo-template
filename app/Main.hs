f04f0fbb9363cf25f75f750adfe1a379dd565cf3
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-09 16:36:11 -0400

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -28,6 +28,33 @@ printLogo = do
     putStrLn "                         ~-_           _-~          ~-_       _-~    "
     putStrLn "                            ~--______-~                ~-___-~       "   
     putStrLn "                                                                     "
+    
+    
+    printLogo :: IO ()
+printLogo = do
+    putStrLn "                        /^\/^\                                       "
+    putStrLn "                      _|__|  O|                                      "
+    putStrLn "             \/     /~     \_/ \                                     "
+    putStrLn "              \____|__________/  \                                   "
+    putStrLn "                     \_______      \                                 "
+    putStrLn "                             `\     \                                "
+    putStrLn "                               |     |                               "                  
+    putStrLn "       ,--./,-.                {     }                               "
+    putStrLn "      /# SNAKE  \              {     }                               "
+    putStrLn "     |   GAME   |              {     }                               "         
+    putStrLn "      \        /               |     |                               "
+    putStrLn "       `._,._,'               /     /                    \           "
+    putStrLn "                             /     /                       \\        "
+    putStrLn "                           /      /                         \ \      "
+    putStrLn "                          /     /                            \  \    "
+    putStrLn "                         /     /             _----_            \   \ "
+    putStrLn "                        /     /           _-~      ~-_         |   | "
+    putStrLn "                       (      (        _-~    _--_    ~-_     _/   | "
+    putStrLn "                        \      ~-____-~    _-~    ~-_    ~-_-~    /  "
+    putStrLn "                         ~-_           _-~          ~-_       _-~    "
+    putStrLn "                            ~--______-~                ~-___-~       "   
+    putStrLn "                                                                     "
+
                                                                                        
 
    
