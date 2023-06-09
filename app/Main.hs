c4307ae7eaa0a5458c0f910e46acc8c14b3f608d
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-09 16:41:53 -0400

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -30,30 +30,15 @@ printLogo = do
     putStrLn "                                                                     "
     
     
-    printLogo :: IO ()
-printLogo = do
-    putStrLn "                        /^\/^\                                       "
-    putStrLn "                      _|__|  O|                                      "
-    putStrLn "             \/     /~     \_/ \                                     "
-    putStrLn "              \____|__________/  \                                   "
-    putStrLn "                     \_______      \                                 "
-    putStrLn "                             `\     \                                "
-    putStrLn "                               |     |                               "                  
-    putStrLn "       ,--./,-.                {     }                               "
-    putStrLn "      /# SNAKE  \              {     }                               "
-    putStrLn "     |   GAME   |              {     }                               "         
-    putStrLn "      \        /               |     |                               "
-    putStrLn "       `._,._,'               /     /                    \           "
-    putStrLn "                             /     /                       \\        "
-    putStrLn "                           /      /                         \ \      "
-    putStrLn "                          /     /                            \  \    "
-    putStrLn "                         /     /             _----_            \   \ "
-    putStrLn "                        /     /           _-~      ~-_         |   | "
-    putStrLn "                       (      (        _-~    _--_    ~-_     _/   | "
-    putStrLn "                        \      ~-____-~    _-~    ~-_    ~-_-~    /  "
-    putStrLn "                         ~-_           _-~          ~-_       _-~    "
-    putStrLn "                            ~--______-~                ~-___-~       "   
-    putStrLn "                                                                     "
+    main :: IO ()
+main = do
+    hSetBuffering stdin NoBuffering
+    hSetEcho stdin False
+    clearScreen
+    printLogo
+    commands <- newTQueueIO
+    worldVar <- newTVarIO (initGame $ mkStdGen 0)
+    withAsync (commandLoop commands) $ \_ -> gameLoop commands worldVar
 
                                                                                        
 
