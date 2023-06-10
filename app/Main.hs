eb141c1f097984cde0aae3e75f99d6ce00171842
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-10 13:02:09 -0400

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -4,32 +4,7 @@ import Actions
 import Lib
 import Types
 
-printLogo :: IO ()
-printLogo = do
-    putStrLn "                        /^\/^\                                       "
-    putStrLn "                      _|__|  O|                                      "
-    putStrLn "             \/     /~     \_/ \                                     "
-    putStrLn "              \____|__________/  \                                   "
-    putStrLn "                     \_______      \                                 "
-    putStrLn "                             `\     \                                "
-    putStrLn "                               |     |                               "                  
-    putStrLn "       ,--./,-.                {     }                               "
-    putStrLn "      /# SNAKE  \              {     }                               "
-    putStrLn "     |   GAME   |              {     }                               "         
-    putStrLn "      \        /               |     |                               "
-    putStrLn "       `._,._,'               /     /                    \           "
-    putStrLn "                             /     /                       \\        "
-    putStrLn "                           /      /                         \ \      "
-    putStrLn "                          /     /                            \  \    "
-    putStrLn "                         /     /             _----_            \   \ "
-    putStrLn "                        /     /           _-~      ~-_         |   | "
-    putStrLn "                       (      (        _-~    _--_    ~-_     _/   | "
-    putStrLn "                        \      ~-____-~    _-~    ~-_    ~-_-~    /  "
-    putStrLn "                         ~-_           _-~          ~-_       _-~    "
-    putStrLn "                            ~--______-~                ~-___-~       "   
-    putStrLn "                                                                     "
-    
-    
+
     main :: IO ()
 main = do
     hSetBuffering stdin NoBuffering
