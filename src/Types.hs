f5d9cef3812281f6f5297c8c33730d8d9e7aea7a
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-10 13:24:14 -0400

    Update Types.hs

--- src/Types.hs
+++ src/Types.hs
@@ -1,1 +1,27 @@
 module Types where
+
+import System.Random
+
+
+data Direction = Up | Down | Left | Right deriving (Eq, Enum)
+
+
+data Command = Exit | Move Direction deriving (Eq)
+
+
+type Position = (Int, Int)
+
+
+type Snake = [Position]
+
+
+data World = World { 
+                    snake :: Snake,
+                    food :: Position,
+                    direction :: Direction,
+                    randomGen :: StdGen,
+                    bounds :: (Int, Int)
+                   }
+
+
+data GameState = Game { world :: World, score :: Int } | GameOver Int
