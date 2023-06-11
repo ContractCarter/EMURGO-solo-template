3dd7c6a3cd0b24b7615881ce666e511d9eb23fe2
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-10 22:57:09 -0400

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
 import Actions
 import Lib
 import Types
-
+import System.IO
+import System.Console.ANSI
+import System.Random
+import Control.Concurrent.STM
+import Control.Concurrent.Async
 
     main :: IO ()
     main = do
