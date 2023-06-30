348698720f6d16dc156cd253e04f0baccb8559d2
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-30 10:03:47 -0400

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,56 +6,53 @@ import System.Random
 
 move :: Direction -> Position -> Position
 move dir (r, c) = case dir of
-    U -> (r - 1, c)
-    D -> (r + 1, c)
-    L -> (r, c - 1)
-    R -> (r, c + 1)
+  U -> (r - 1, c)
+  D -> (r + 1, c)
+  L -> (r, c - 1)
+  R -> (r, c + 1)
 
 advance :: GameState -> Command -> GameState
-advance (Game world@World{snake, food, direction, randomGen, bounds} score) cmd = case cmd of
+advance (Game world@World{snake, food, direction, randomGen, bounds} score) cmd =
+  case cmd of
     Exit -> GameOver score
-    Move dir -> if collision then GameOver score else newGame
-        where
-          newDirection = if dir == opposite direction then direction else dir
-          newHead = move newDirection (head snake)
-          collision = newHead `elem` snake || outOfBounds bounds newHead
-          (newSnake, newFood, newRand) = if newHead == food
-                                         then let (newFoodPosition, newGen) = randomPosition bounds randomGen 
-                                              in (newHead : snake, newFoodPosition, newGen)
-                                         else (newHead : init snake, food, randomGen)
-          newGame = Game (world {snake = newSnake, food = newFood, direction = newDirection, randomGen = newRand}) (score + length newSnake)
+    Move dir ->
+      if any (\seg -> move direction (head snake) == seg) (tail snake) || outOfBounds bounds (move dir (head snake))
+        then GameOver score
+        else let newSnake = if move dir (head snake) == food
+                              then food : snake
+                              else init snake
+                 (newFood, newRand) = if move dir (head snake) == food
+                                        then randomPosition bounds randomGen
+                                        else (food, randomGen)
+              in Game (world { snake = move dir (head snake) : newSnake
+                             , food = newFood
+                             , direction = dir
+                             , randomGen = newRand
+                             }) (score + length newSnake)
 advance game _ = game
 
-opposite :: Direction -> Direction
-opposite dir = toEnum $ (fromEnum dir + 2) `mod` 4
-
 randomPosition :: (Int, Int) -> StdGen -> (Position, StdGen)
-randomPosition (maxr, maxc) g = 
-    let (r, g1) = randomR (1, maxr) g
-        (c, g2) = randomR (1, maxc) g1
-    in ((r, c), g2)
-
+randomPosition (maxr, maxc) g =
+  let (r, g1) = randomR (1, maxr) g
+      (c, g2) = randomR (1, maxc) g1
+  in ((r, c), g2)
 
 outOfBounds :: (Int, Int) -> Position -> Bool
 outOfBounds (maxr, maxc) (r, c) = r < 1 || r > maxr || c < 1 || c > maxc
 
 initWorld :: StdGen -> World
-initWorld g = World { snake = [(10, x)| x <- [20,19..15]],
-                      food = (10, 10),
-                      direction = R,
-                      randomGen = g,
-                      bounds = (20, 40)
-                    }
+initWorld g = World {
+  snake = [(10, x) | x <- [20, 19 .. 15]],
+  food = (10, 10),
+  direction = R,
+  randomGen = g,
+  bounds = (40, 80)
+}
 
 initGame :: StdGen -> GameState
 initGame g = Game (initWorld g) 0
 
-
 parseCommand :: Char -> Maybe Command
-parseCommand ch = case ch of
-    'q' -> Just Exit
-    'w' -> Just $ Move U
-    'a' -> Just $ Move L
-    's' -> Just $ Move D
-    'd' -> Just $ Move R
-    _   -> Nothing
+parseCommand ch = lookup ch commands
+  where
+    commands = [('q', Exit), ('w', Move U), ('a', Move L), ('s', Move D), ('d', Move R)]
