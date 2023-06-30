c2fa38cb7bbddbc57234e2a4633aa1381073f4e8
Lance Carter <88419641+ContractCarter@users.noreply.github.com>
2023-06-30 10:01:48 -0400

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -15,18 +15,18 @@ import Control.Concurrent
 
 main :: IO ()
 main = do
-    hSetBuffering stdin NoBuffering
-    hSetEcho stdin False
-    clearScreen
-    printLogo
-    putStrLn "Press any key to start..."
-    _ <- getChar
-    clearScreen
-    commands <- newTQueueIO
-    worldVar <- newTVarIO (initGame $ mkStdGen 0)
-    withAsync (commandLoop commands) $ \_ -> 
-      withAsync (gameLoop commands worldVar) $ \_ ->
-        forever (threadDelay 100000)
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  clearScreen
+  printLogo
+  putStrLn "Press any key to start..."
+  _ <- getChar
+  clearScreen
+  commands <- newTQueueIO
+  worldVar <- newTVarIO (initGame $ mkStdGen 0)
+  withAsync (commandLoop commands) $ \_ ->
+    withAsync (gameLoop commands worldVar) $ \_ ->
+      forever (threadDelay 100000)
 
                                                                                        
 
